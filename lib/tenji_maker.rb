e57d14a8ff453159e71a3de5327d9ae7c22d5d71
YAMASHITA,Junji <ysiijj+github@gmail.com>
2021-12-30 20:07:48 +0900

    辛口バージョン対応

--- lib/tenji_maker.rb
+++ lib/tenji_maker.rb
@@ -4,7 +4,7 @@
 class TenjiMaker
   def to_tenji(text)
     text.split(' ')
-        .map { |char| to_bits(char) }
+        .flat_map { |char| to_bits(char) }
         .map { |bits| to_tenji_array(bits) }
         .transpose
         .map { |line| line.join(' ') }
@@ -35,11 +35,17 @@ class TenjiMaker
   private
 
   def to_bits(char)
-    # char を(子)(母)に分割
-    # (子)(母)の組み合わせからビットパターンを生成
-    /^(.)?(.)$/.match(char)
-               .captures
-               .then { |c1, c2| SIIN[c1][BOIN[c2]] }
+    # char を(子)(促)(拗)(母)|(単)に分割
+    # 分割結果の組み合わせからビットパターンを生成
+    /^(?:(.)?(\1)?(Y)?([AIUEO])|([-N]))$/
+      .match(char).captures
+      .then do |si, so, yo, bo, ta|
+        [
+          sokuon_if(so),
+          inject_yoon_if(yo, FUKU[si][TAN[bo || ta]])
+        ]
+      end
+      .compact.flatten
   end
 
   def to_tenji_array(bits)
@@ -49,15 +55,16 @@ class TenjiMaker
   # http://www.naiiv.net/braille/?tenji-sikumi
   # ビット列は文字の出力順に合わせており、
   # 上記サイトの (1)(4)_(2)(5)_(3)(6) の並びに相当する。
-  BOIN = {
+  TAN = {
     'A' => 0b10_00_00,
     'I' => 0b10_10_00,
     'U' => 0b11_00_00,
     'E' => 0b11_10_00,
     'O' => 0b01_10_00,
-    'N' => 0b00_01_11
+    'N' => 0b00_01_11,
+    '-' => 0b00_11_00
   }.freeze
-  SIIN = {
+  FUKU = {
     nil => ->(b) { b },
     'K' => ->(b) { b | 0b00_00_01 },
     'S' => ->(b) { b | 0b00_01_01 },
@@ -67,14 +74,42 @@ class TenjiMaker
     'M' => ->(b) { b | 0b00_01_11 },
     'R' => ->(b) { b | 0b00_01_00 },
     'Y' => ->(b) { shift(b) | 0b01_00_00 },
-    'W' => ->(b) { shift(b) | 0b00_00_00 }
+    'W' => ->(b) { shift(b) | 0b00_00_00 },
+    'G' => ->(b) { dakuon('K', b) },
+    'Z' => ->(b) { dakuon('S', b) },
+    'D' => ->(b) { dakuon('T', b) },
+    'B' => ->(b) { dakuon('H', b) },
+    'P' => ->(b) { handakuon('H', b) }
   }.freeze
 
-  def self.shift(bits)
-    # 0b00_11_00 との & で 2段目のデータ有無を判定し、
-    # 2段目があれば 1行(2ビット)だけシフト、
-    # 2段目がなければ 2行(4ビット)だけシフトする
-    bits >> ((bits & 0b00_11_00).positive? ? 2 : 4)
+  class << self
+    private
+
+    def shift(bits)
+      # 0b00_11_00 との & で 2段目のデータ有無を判定し、
+      # 2段目があれば 1行(2ビット)だけシフト、
+      # 2段目がなければ 2行(4ビット)だけシフトする
+      bits >> ((bits & 0b00_11_00).positive? ? 2 : 4)
+    end
+
+    def dakuon(fuku, tan)
+      [0b00_01_00, FUKU[fuku][tan]]
+    end
+
+    def handakuon(fuku, tan)
+      [0b00_00_01, FUKU[fuku][tan]]
+    end
   end
-  private_class_method :shift
+
+  def sokuon_if(soku)
+    soku ? 0b00_10_00 : nil
+  end
+
+  def inject_yoon_if(yoo, pair)
+    if yoo
+      pair = [0, pair] unless pair.is_a?(Array)
+      pair[0] |= 0b01_00_00
+    end
+    pair
+  end
 end
